a1113bf116f50ecd45e0c7efef669332e934c577
Shark1906 <amanraj1906@gmail.com>
2021-11-17 00:27:46 +0530

    ADDED : UC 2

--- src/com/maximum/Main.java
+++ src/com/maximum/Main.java
@@ -3,18 +3,18 @@ package com.maximum;
 public class Main {
 	
 	public static void main(String[] args) {
-		//Test Case 1.1
-			TestMaximum maximum1 = new TestMaximum(30, 15, 20);
+		//Test Case 2.1
+			TestMaximum maximum1 = new TestMaximum(30f, 15f, 20f);
 			maximum1.returnMax();
 			System.out.println(maximum1);
 		
-		//Test Case 1.2
-			TestMaximum maximum2 = new TestMaximum(15, 45, 20);
+		//Test Case 2.2
+			TestMaximum maximum2 = new TestMaximum(15f, 45f, 20f);
 			maximum2.returnMax();
 			System.out.println(maximum2);
 				
-		//Test Case 1.3
-			TestMaximum maximum3 = new TestMaximum(30, 15, 55);
+		//Test Case 2.3
+			TestMaximum maximum3 = new TestMaximum(30f, 15f, 55f);
 			maximum3.returnMax();
 			System.out.println(maximum3);
 		
